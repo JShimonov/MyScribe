184844b6e432748e7affcd178eca41e0c8d743f3
brpasiliao <brpasiliao@gmail.com>
2021-11-27 23:51:12 -0500

    driver class laid out & added some classes for strategy pattern

---
+++ src/JumpToBeginning.java
@@ -0,0 +1,9 @@
+public class JumpToBeginning implements JumpStrategy {
+    public JumpToBeginning() {
+        
+    }
+
+    public void jumpTo(int time) {
+        System.out.println("beginning: " + time);
+    }
+}
